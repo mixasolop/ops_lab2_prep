b3d0e65f06a7dbc0e9b61a0a07aebfcf6a10d406
Igor_Goluch <mixasolop@gmail.com>
2025-12-04 18:44:20 +0100

    first part

--- sop-collection.c
+++ sop-collection.c
@@ -9,72 +9,102 @@
 #include <time.h>
 #include <unistd.h>
 
-#define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), kill(0, SIGKILL), exit(EXIT_FAILURE))
+#define ERR(source)                                                            \
+  (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__),             \
+   kill(0, SIGKILL), exit(EXIT_FAILURE))
 
 #define ITER_COUNT 25
 
-ssize_t bulk_read(int fd, char* buf, size_t count)
-{
-    ssize_t c;
-    ssize_t len = 0;
-    do
-    {
-        c = TEMP_FAILURE_RETRY(read(fd, buf, count));
-        if (c < 0)
-            return c;
-        if (c == 0)
-            return len;  // EOF
-        buf += c;
-        len += c;
-        count -= c;
-    } while (count > 0);
-    return len;
+ssize_t bulk_read(int fd, char *buf, size_t count) {
+  ssize_t c;
+  ssize_t len = 0;
+  do {
+    c = TEMP_FAILURE_RETRY(read(fd, buf, count));
+    if (c < 0)
+      return c;
+    if (c == 0)
+      return len; // EOF
+    buf += c;
+    len += c;
+    count -= c;
+  } while (count > 0);
+  return len;
 }
 
-ssize_t bulk_write(int fd, char* buf, size_t count)
-{
-    ssize_t c;
-    ssize_t len = 0;
-    do
-    {
-        c = TEMP_FAILURE_RETRY(write(fd, buf, count));
-        if (c < 0)
-            return c;
-        buf += c;
-        len += c;
-        count -= c;
-    } while (count > 0);
-    return len;
+ssize_t bulk_write(int fd, char *buf, size_t count) {
+  ssize_t c;
+  ssize_t len = 0;
+  do {
+    c = TEMP_FAILURE_RETRY(write(fd, buf, count));
+    if (c < 0)
+      return c;
+    buf += c;
+    len += c;
+    count -= c;
+  } while (count > 0);
+  return len;
 }
 
-void sethandler(void (*f)(int), int sigNo)
-{
-    struct sigaction act;
-    memset(&act, 0, sizeof(struct sigaction));
-    act.sa_handler = f;
-    if (-1 == sigaction(sigNo, &act, NULL))
-        ERR("sigaction");
+void sethandler(void (*f)(int), int sigNo) {
+  struct sigaction act;
+  memset(&act, 0, sizeof(struct sigaction));
+  act.sa_handler = f;
+  if (-1 == sigaction(sigNo, &act, NULL))
+    ERR("sigaction");
 }
 
-void ms_sleep(unsigned int milli)
-{
-    time_t sec = (int)(milli / 1000);
-    milli = milli - (sec * 1000);
-    struct timespec ts = {0};
-    ts.tv_sec = sec;
-    ts.tv_nsec = milli * 1000000L;
-    if (TEMP_FAILURE_RETRY(nanosleep(&ts, &ts)))
-        ERR("nanosleep");
+void ms_sleep(unsigned int milli) {
+  time_t sec = (int)(milli / 1000);
+  milli = milli - (sec * 1000);
+  struct timespec ts = {0};
+  ts.tv_sec = sec;
+  ts.tv_nsec = milli * 1000000L;
+  if (TEMP_FAILURE_RETRY(nanosleep(&ts, &ts)))
+    ERR("nanosleep");
 }
 
-void usage(int argc, char* argv[])
-{
-    printf("%s n\n", argv[0]);
-    printf("\t1 <= n <= 4 -- number of moneyboxes\n");
-    exit(EXIT_FAILURE);
+void usage(int argc, char *argv[]) {
+  printf("%s n\n", argv[0]);
+  printf("\t1 <= n <= 4 -- number of moneyboxes\n");
+  exit(EXIT_FAILURE);
 }
 
-int main(int argc, char* argv[])
-{
-    return EXIT_SUCCESS;
+void child_work() {
+  printf("[%d] Collection box opened\n", getpid());
+  char buf[69];
+  snprintf(buf, 67, "skarbona_%d", getpid());
+  int fd = open(buf, O_CREAT | O_TRUNC);
+  if (fd == -1) {
+    ERR("open");
+  }
+  close(fd);
+}
+
+void create_n_children(int n, pid_t *arr) {
+  for (int i = 0; i < n; i++) {
+    pid_t pid = fork();
+    arr[i] = pid;
+    if (pid == -1) {
+      ERR("fork");
+    }
+    if (pid == 0) {
+      child_work();
+      exit(EXIT_SUCCESS);
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    usage(argc, argv);
+  }
+  int num = atoi(argv[1]);
+  if (num < 1 || num > 4) {
+    usage(argc, argv);
+  }
+  pid_t children_pid[num];
+  create_n_children(num, children_pid);
+  while (wait(NULL) > 0) {
+  }
+  printf("Collection ended!");
 }
